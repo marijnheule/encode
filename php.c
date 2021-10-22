3ab5608be5b18a5519f268b32c4503c2c294e47d
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2021-10-22 14:10:55 -0400

    c file

---
+++ php.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv) {
+  int size = atoi (argv[1]);
+  int nVar = size * (size - 1);
+  int nCls = size + size * (size - 1) * (size - 1) / 2;
+
+  printf("p cnf %i %i\n", nVar, nCls);
+
+  for (int i = 0; i < size; ++i) {
+    for (int j = 1; j < size; ++j)
+      printf("%i ", i * (size - 1) + j);
+    printf("0\n"); }
+
+  for (int i = 1; i < size; ++i)
+    for (int j = 0; j < size; ++j)
+      for (int k = j + 1; k < size; ++k)
+        printf("-%i -%i 0\n", j * (size-1) + i, k * (size-1) + i);
+}
