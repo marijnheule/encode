d7af6f73ab3a7f4a8f28114dd917401dc1857b13
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2022-11-09 11:29:48 -0500

    more compact Green encoding

--- Green.c
+++ Green.c
@@ -1,42 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 int main (int argc, char** argv) {
 
   if (argc < 3) {
-    printf ("c too few arguments: %s size discrepancy\n", argv[1]);
+    printf ("c too few arguments: %s k (discrepancy) n (length)\n", argv[1]);
     exit (0); }
 
-  int size = atoi (argv[1]);
-  int disc = atoi (argv[2]);
+  int k = atoi (argv[1]);
+  int n = atoi (argv[2]);
 
-  int nVar = size;
   int nCls = 0;
+  for (int i = 1; i < n; i++)
+    for (int d = 1; d < n; d++) {
+      if (i + 2*d <= n) nCls++;
+      if (i + (k-1)*d <= n) nCls++; }
 
-  for (int i = 1; i < size; i++)
-    for (int d = 1; d < size; d++)
-      if (i + 2*d <= size)
-        nCls++;
+  printf ("p cnf %i %i\n", n, nCls);
 
-  for (int i = 1; i < size; i++)
-    for (int d = 1; d < size; d++)
-      if (i + (disc-1)*d <= size)
-        nCls++;
+  for (int i = 1; i < n; i++)
+    for (int d = 1; d < n; d++)
+      if (i + (k-1)*d <= n) {
+        for (int j = 0; j < k; j++)
+          printf ("%i ", i + j*d);
+        printf ("0\n"); }
 
-  printf ("p cnf %i %i\n", nVar, nCls);
-
-  for (int i = 1; i < size; i++)
-    for (int d = 1; d < size; d++)
-      if (i + 2*d <= size)
-        printf ("%i %i %i 0\n", i, i+d, i+2*d);
-
-  for (int i = 1; i < size; i++)
-    for (int d = 1; d < size; d++)
-      if (i + (disc-1)*d <= size) {
-        for (int l = 0; l < disc; l++)
-          printf ("%i ", -i - l*d);
-        printf ("0\n");
-      }
-
+  for (int i = 1; i < n; i++)
+    for (int d = 1; d < n; d++)
+      if (i + 2*d <= n)
+        printf ("-%i -%i -%i 0\n", i, i+d, i+2*d);
 }
